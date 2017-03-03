fb951a5347e47b4c31e04dbb9895b414968343f2
Tom Alves <elitonavds@hotmail.com>
2017-03-03 08:56:43 -0300

    criando o projeto

---
+++ app/src/main/java/petshop/rm77283/desafio2/fiap/com/br/petshop/MainActivity.java
@@ -0,0 +1,13 @@
+package petshop.rm77283.desafio2.fiap.com.br.petshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
