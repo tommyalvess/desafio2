1768df1c6c8a1c2fec5d9ed78d92e601ee118a04
Tom Alves <elitonavds@hotmail.com>
2017-03-03 12:10:41 -0300

    refazendo validacao

--- app/src/main/java/petshop/rm77283/desafio2/fiap/com/br/petshop/MainActivity.java
+++ app/src/main/java/petshop/rm77283/desafio2/fiap/com/br/petshop/MainActivity.java
@@ -1,18 +1,26 @@
 package petshop.rm77283.desafio2.fiap.com.br.petshop;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
 
     CheckBox femea, adestrado, vacina;
-    RadioButton c1;
+
+
     TextView resultado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +30,20 @@ public class MainActivity extends AppCompatActivity {
         femea = (CheckBox) findViewById(R.id.femea);
         adestrado = (CheckBox) findViewById(R.id.adestrado);
         vacina = (CheckBox) findViewById(R.id.vacina);
-        resultado =(TextView) findViewById(R.id.calculcar);
+        resultado = (TextView) findViewById(R.id.calculcar);
         c1 = (RadioButton) findViewById(R.id.c1);
 
-        Button btncalcular = (Button) findViewById(R.id.btncalcular);
 
-        btncalcular.setOnClickListener(new View.OnClickListener() {
+    }
 
-
-            @Override
-            public void onClick(View v) {
+            public void botao(View v) {
 
                 double resultado = 0;
-                if(c1.isChecked() && femea.isChecked()){
+                if (c1.isChecked() && femea.isChecked()) {
                     resultado = 800.00 + 180.00;
                 }
 
             }
-        });
-        };
 
 
 
